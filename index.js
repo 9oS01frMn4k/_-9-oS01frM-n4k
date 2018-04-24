7ab70ac1f81c208306f47ab3bff1d0fc1cf41438
9oS01frMn4k <38668261+9oS01frMn4k@users.noreply.github.com>
2018-04-24 16:50:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,32 @@ client.on("message", async message => {
  		.setDescription(args.join(" "));
  		message.channel.send({embed})
 }
+  if(cmd === `${prefix}report`){
+
+    //!report @ned this is the reason
+
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "reports");
+    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+    return;
+  }
 
 
    if(cmd === `${prefix}botinfo`){
