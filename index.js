471c485aa2810793c611157c275092735ecac5c1
9oS01frMn4k <38668261+9oS01frMn4k@users.noreply.github.com>
2018-04-27 14:40:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("message", async message => {
 
 
 
-   if(cmd === `${prefix}serverinfo`){
+  if(cmd === `${prefix}serverinfo`){
 
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
@@ -42,7 +42,6 @@ client.on("message", async message => {
      .addField("Members", message.guild.memberCount);
 
    return message.channel.send(serverembed);
- }
 
  if (cmd === `${prefix}say`){
  		message.delete()
@@ -51,6 +50,32 @@ client.on("message", async message => {
  		.setDescription(args.join(" "));
  		message.channel.send({embed})
 }
+  if(cmd === `${prefix}report`){
+
+    //!report @ned this is the reason
+
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "reports");
+    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+    return;
+  }
 
 
    if(cmd === `${prefix}botinfo`){
@@ -65,37 +90,19 @@ client.on("message", async message => {
 
      return message.channel.send(botembed);
    }
+ if (cmd === `${prefix}invite`){
+   message.reply('Invite the bot to your server :wink: https://discordapp.com/oauth2/authorize?client_id=433340501111078922&scope=bot&permissions=20972552');
+ }
 
-
-
-   if (cmd === `${prefix}report`){
-   var reportchannel = client.channels.get('435862363158085652');
-             var reporteduser = message.mentions.users.first().id;
-             var reportreason = message.content.split(' ').slice(3).join(' ');
-
-             if (!message.channel.id == '435861980079849472') {
-              return message.reply(`Please report someone in the \`reports\` channel!`);
-             }
-
-             if (message.author.id === reporteduser) {
-                 return message.reply('You cant punish yourself :wink:')
-             }
-
-             if (message.mentions.users.size < 1 || message.mentions.users.size > 1) {
-                 return message.reply('You need to mention someone to report him!')
-             }
-
-             reportchannel.send(`Maniak: ${message.author.tag}\nReported user: ${reporteduser}\nReason: ${reportreason}`);
-
-             message.reply(`We got your report! Thanks :heart:`);
-   }
-  
+   if (cmd === `${prefix}help`){
+   message.reply('Please check your direct messages :mailbox_with_no_mail:');
    message.author.send(`${prefix}serverinfo - info about the server\n\
 ${prefix}report - report someone for breaking the server rules
 ${prefix}botinfo - info about the bot
 ${prefix}moveall (room) (-mute - if you want) - move members - only staff
-${prefix}say (text) - The bot says what you say`);
-   });
+${prefix}say (text) - The bot says what you say
+${prefix}invite - invite the bot to your server by .invite`);
+   }
 
 if (cmd === `${prefix}moveall`){
   let isAdmin = message.member.roles.filterArray(role => {return role.name === 'Owner' || role.name === 'Move-all-er';}).length;
@@ -104,7 +111,7 @@ if (cmd === `${prefix}moveall`){
   }
   if (message.content.indexOf(".moveall") > -1) {
     channelGetName = message.content.slice(9, 9999);
-    findChannel = client.channels.find('name', channelGetName);
+    findChannel = bot.channels.find('name', channelGetName);
     if (message.content.indexOf("-mute") > -1) {
       MoveMuteUsers(findChannel);
     } else{
@@ -141,9 +148,14 @@ function MoveMuteUsers(findChannel){
 });
 }
 
-
 // * Move from specific channels.
 // * ignore specific users.
 
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
