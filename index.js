2323c1cc5f214bce6b1e8cd5c44457fb03fd260c
9oS01frMn4k <38668261+9oS01frMn4k@users.noreply.github.com>
2018-04-27 14:42:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Bot is Online!`);
-  client.user.setGame(`${client.guilds.size} servers | .help`);
-});
-
-// Updates the bot's status if he joins a server
-client.on("guildCreate", guild => {
-   client.user.setGame(`${client.guilds.size} servers | .help`);
-});
-
-/// Updates the bot's status if he leaves a servers
-client.on("guildDelete", guild => {
-    client.user.setGame(
-        `${client.guilds.size} servers | .help`);
+  client.user.setGame(".help | Bot By NiceGames & loltrolol55");
 });
 
 client.on("message", async message => {
@@ -36,21 +25,33 @@ client.on("message", async message => {
      .setDescription("Server Information")
      .setColor("#15f153")
      .setThumbnail(sicon)
-     .addField("Server Name", message.guild.name)
+     .addField("שם השרת", message.guild.name)
      .addField("Created On", message.guild.createdAt)
-     .addField("Joined At", message.member.joinedAt)
-     .addField("Members", message.guild.memberCount);
+     .addField("הצטרפת ב", message.member.joinedAt)
+     .addField("אנשים בטירה", message.guild.memberCount);
 
    return message.channel.send(serverembed);
+ }
 
- if (cmd === `${prefix}say`){
- 		message.delete()
-         const embed = new Discord.RichEmbed()
- 		.setColor(0x954D23)
- 		.setDescription(args.join(" "));
- 		message.channel.send({embed})
-}
-  if(cmd === `${prefix}report`){
+
+
+
+   if(cmd === `${prefix}botinfo`){
+
+   let bicon = client.user.displayAvatarURL;
+   let botembed = new Discord.RichEmbed()
+   .setDescription("Bot Informtaion")
+   .setColor("#15f153")
+   .setThumbnail(bicon)
+   .addField("Bot Name", client.user.username)
+   .addField("Created On", client.user.createdAt);
+
+     return message.channel.send(botembed);
+   }
+
+
+
+    if(cmd === `${prefix}report`){
 
     //!report @ned this is the reason
 
@@ -75,81 +76,62 @@ client.on("message", async message => {
     reportschannel.send(reportEmbed);
 
     return;
-  }
-
-
-   if(cmd === `${prefix}botinfo`){
-
-   let bicon = client.user.displayAvatarURL;
-   let botembed = new Discord.RichEmbed()
-   .setDescription("Bot Informtaion")
-   .setColor("#15f153")
-   .setThumbnail(bicon)
-   .addField("Bot Name", client.user.username)
-   .addField("Created On", client.user.createdAt);
-
-     return message.channel.send(botembed);
    }
- if (cmd === `${prefix}invite`){
-   message.reply('Invite the bot to your server :wink: https://discordapp.com/oauth2/authorize?client_id=433340501111078922&scope=bot&permissions=20972552');
- }
-
    if (cmd === `${prefix}help`){
-   message.reply('Please check your direct messages :mailbox_with_no_mail:');
+   message.reply('שולח לך בפרטי נודר');
+
    message.author.send(`${prefix}serverinfo - info about the server\n\
 ${prefix}report - report someone for breaking the server rules
-${prefix}botinfo - info about the bot
-${prefix}moveall (room) (-mute - if you want) - move members - only staff
-${prefix}say (text) - The bot says what you say
-${prefix}invite - invite the bot to your server by .invite`);
+${prefix}botinfo - info about the bot`);
    }
 
-if (cmd === `${prefix}moveall`){
-  let isAdmin = message.member.roles.filterArray(role => {return role.name === 'Owner' || role.name === 'Move-all-er';}).length;
-  if (isAdmin === 0){
-    return;
-  }
-  if (message.content.indexOf(".moveall") > -1) {
-    channelGetName = message.content.slice(9, 9999);
-    findChannel = bot.channels.find('name', channelGetName);
-    if (message.content.indexOf("-mute") > -1) {
-      MoveMuteUsers(findChannel);
-    } else{
-      MoveUsers(findChannel);
-    }
-  }
-}
-});
+   if (cmd === `${prefix}moveall`){
+     let isAdmin = message.member.roles.filterArray(role => {return role.name === 'Owner' || role.name === 'Move-all-er';}).length;
+     if (isAdmin === 0){
+       return;
+     }
+     if (message.content.indexOf(".moveall") > -1) {
+       channelGetName = message.content.slice(9, 9999);
+       findChannel = bot.channels.find('name', channelGetName);
+       if (message.content.indexOf("-mute") > -1) {
+         MoveMuteUsers(findChannel);
+       } else{
+         MoveUsers(findChannel);
+       }
+     }
+   }
+   });
 
-function MoveUsers(findChannel){
-  client.channels.findAll('type', 'voice').forEach(channelInfo => {
-    if (channelInfo.name.indexOf("AFK") > -1 ){
-      console.log("afk");
-    } else {
-      channelInfo.members.array().forEach(memberNumber => {
-        memberNumber.setVoiceChannel(findChannel);
-        console.log('moving');
-        });
-    }
-});
-}
+   function MoveUsers(findChannel){
+     bot.channels.findAll('type', 'voice').forEach(channelInfo => {
+       if (channelInfo.name.indexOf("AFK") > -1 ){
+         console.log("afk");
+       } else {
+         channelInfo.members.array().forEach(memberNumber => {
+           memberNumber.setVoiceChannel(findChannel);
+           console.log('moving');
+           });
+       }
+   });
+   }
 
-function MoveMuteUsers(findChannel){
-  client.channels.findAll('type', 'voice').forEach(channelInfo => {
-    if (channelInfo.name.indexOf("AFK") > -1 ){
-      console.log("afk");
-    } else {
-      channelInfo.members.array().forEach(memberNumber => {
-        memberNumber.setVoiceChannel(findChannel);
-        memberNumber.setMute(true, 'moveall');
-        console.log('moving');
-        });
-    }
-});
-}
+   function MoveMuteUsers(findChannel){
+     bot.channels.findAll('type', 'voice').forEach(channelInfo => {
+       if (channelInfo.name.indexOf("AFK") > -1 ){
+         console.log("afk");
+       } else {
+         channelInfo.members.array().forEach(memberNumber => {
+           memberNumber.setVoiceChannel(findChannel);
+           memberNumber.setMute(true, 'moveall');
+           console.log('moving');
+           });
+       }
+   });
+   }
 
-// * Move from specific channels.
-// * ignore specific users.
+
+   // * Move from specific channels.
+   // * ignore specific users.
 
 
 client.on('message', msg => {
